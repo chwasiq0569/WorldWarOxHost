f078847d9c25de89ce03c0bcdf3166c9d48f84d5
chwasiq0569 <wasiqabdullah222@gmail.com>
2024-03-10 00:08:24 +0100

    made final touces

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -352,7 +352,7 @@ const HomePage = () => {
                 <div className="TheSoldiersNFTContainerCentered">
                     <div className="thesoldiersNFTBox">
                         <h2>THE SOLDIERS</h2>
-                        <p>Tranche #1 of the Soldier NFTs will be minted February. These OG collectibles kick off the World War 0x ecosystem - boosting 3D files, in-game accessibility and integration into our betting protocol and  map creator tools.</p>
+                        <p>Tranche #1 of the Soldier NFTs will be minted March. These OG collectibles kick off the World War 0x ecosystem - boosting 3D files, in-game accessibility and integration into our betting protocol and  map creator tools.</p>
                     </div>
 
                     <div className="thesoldiersNFTBox">
