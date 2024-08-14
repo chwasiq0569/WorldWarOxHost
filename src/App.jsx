5c1f058798110409c7e1b288ca5aabfecf9dda03
chwasiq0569 <wasiqabdullah222@gmail.com>
2024-08-14 13:16:46 +0200

    Created SelectToken SCreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
 import Privacy from "./pages/Privacy/Privacy";
 import Terms from "./pages/Terms/Terms";
@@ -9,16 +9,17 @@ import TestMintNFTs from "./pages/MintNFT/TestMintNFTs";
 import Support from "./pages/Support/Support";
 import WhiteListPage from "./pages/WhiteListPage/WhiteListPage";
 import TellerATM from "./components/TellerATM/components/TellerATM";
-import {Buffer} from 'buffer';
+import { Buffer } from 'buffer';
 import process from 'process';
+import ChooseToken from './pages/WithdrawScreens/ChooseToken';
 
 window.Buffer = Buffer;
 window.process = process;
 
 function App() {
     return (<Router>
-            <AppContent/>
-        </Router>);
+        <AppContent />
+    </Router>);
 }
 
 function AppContent() {
@@ -29,18 +30,19 @@ function AppContent() {
     }, [location.pathname]);
 
     return (<div className="App">
-            <Routes>
-                <Route exact path="/" element={<HomePage/>}/>
-                <Route exact path="/privacy" element={<Privacy/>}/>
-                <Route exact path="/terms" element={<Terms/>}/>
-                <Route exact path="/download" element={<Download/>}/>
-                <Route exact path="/mintnfts" element={<MintNFT/>}/>
-                <Route exact path="/testmintnfts" element={<TestMintNFTs/>}/>
-                <Route exact path="/support" element={<Support/>}/>
-                <Route exact path="/mint" element={<WhiteListPage/>}/>
-                <Route exact path="/atm" element={<TellerATM/>}/>
-            </Routes>
-        </div>);
+        <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/privacy" element={<Privacy />} />
+            <Route exact path="/terms" element={<Terms />} />
+            <Route exact path="/download" element={<Download />} />
+            <Route exact path="/mintnfts" element={<MintNFT />} />
+            <Route exact path="/testmintnfts" element={<TestMintNFTs />} />
+            <Route exact path="/support" element={<Support />} />
+            <Route exact path="/mint" element={<WhiteListPage />} />
+            {/* <Route exact path="/atm" element={<TellerATM />} /> */}
+            <Route exact path="/atm" element={<ChooseToken />} />
+        </Routes>
+    </div>);
 }
 
 function getBackgroundColor(pathname) {
---
+++ src/pages/WithdrawScreens/ChooseToken.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styles from './choosetoken.module.css'
+import loanIcon from "../../components/TellerATM/icons/teller.png";
+import ReturnButton from '../../components/TellerATM/components/ReturnButton';
+import WW3Token from "../../assets/ww3token.png"
+import BDuckToken from "../../assets/bducktoken.png"
+import WithdrawArrow from "../../assets/witdrawarrow.png"
+
+export default function ChooseToken() {
+
+    const tokens = [{
+        icon: WW3Token,
+        name: 'WW3',
+        rate: '3,591'
+    }, {
+        icon: BDuckToken,
+        name: 'BDUCK',
+        rate: '46,946'
+    }]
+
+    return (
+        <main className={styles.container}>
+            <ReturnButton />
+            <img
+                loading="lazy"
+                src={loanIcon}
+                alt="Teller ATM Logo"
+                className={styles.logo}
+            />
+            <div className={styles.contentWrapper}>
+                <h1 className={styles.welcomeText}>WITHDRAW</h1>
+                <p className={styles.questionText}>CHOOSE WHICH TOKEN TO WITHDRAW</p>
+            </div>
+            <div className={styles.tokens}>
+                {
+                    tokens.map(token => (
+                        <div className={styles.individualToken}>
+                            <img className={styles.tokenImg} src={token.icon} alt="token" />
+                            <div className={styles.tokenDetails}>
+                                <p className={styles.name}>{token.name}</p>
+                                <p className={styles.price}>{token.rate}</p>
+                            </div>
+                            <img className={styles.withdrawArrow} src={WithdrawArrow} alt="WithdrawArrow" />
+                        </div>
+                    ))
+                }
+
+            </div>
+            <section className={styles.optionsWrapper}>
+
+            </section>
+        </main>);
+}
